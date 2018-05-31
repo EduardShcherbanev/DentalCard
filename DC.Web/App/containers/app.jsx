fc223a0f17c80f4d998d215e14abbd66011468ce
Eduard Shcherbanev <fireshock1@gmail.com>
2018-05-31 12:03:31 +0300

    Empty page

---
+++ DC.Web/App/containers/app.jsx
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <h1>Hello</h1>
+                </div>
+            </Router>
+        );
+    }
+};
