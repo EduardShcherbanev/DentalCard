9142aa3844b43d8773e9b61f571d16379820dd9d
Eduard Shcherbanev <fireshock1@gmail.com>
2018-07-03 13:08:22 +0300

    Fix header when scrolling

--- DC.Web/App/containers/header/header.jsx
+++ DC.Web/App/containers/header/header.jsx
@@ -41,24 +41,26 @@ export default class Header extends React.Component {
 
     render() {
         return (
-            <div className="main" style={{ marginBottom: this.state.scrollingLock ? "64px" : "0" }}>
-                <div className="main-overlay">
-                    <div className="header-top hide-on-med-and-down">
-                        <img className="brand-logo responsive-img" src="favicon.png" alt="" />
-                        <div className="right">
-                            <p><a className="white-text" href="tel:+7 926 191-23-94"><Icon tiny>phone</Icon> +7 926 191-23-94</a></p>
-                            <p><a className="white-text" href="mailto:fireshock2@gmail.com"><Icon tiny>email</Icon> fireshock2@gmail.com</a></p>
+            <div style={{ marginBottom: this.state.scrollingLock ? "320px" : "0" }}>
+                <div className="main" style={{ width: "100%", top: this.state.scrollingLock ? "-236px" : "0", position: this.state.scrollingLock ? "fixed" : "relative" }}>
+                    <div className="main-overlay">
+                        <div className="header-top hide-on-med-and-down">
+                            <img className="brand-logo responsive-img" src="favicon.png" alt="" />
+                            <div className="right">
+                                <p><a className="white-text" href="tel:+7 926 191-23-94"><Icon tiny>phone</Icon> +7 926 191-23-94</a></p>
+                                <p><a className="white-text" href="mailto:fireshock2@gmail.com"><Icon tiny>email</Icon> fireshock2@gmail.com</a></p>
+                            </div>
                         </div>
-                    </div>
-                    <Navbar brand="Доктор Диана" right className="header" style={{ width: "100%", top: "0", position: this.state.scrollingLock ? "fixed" : "relative" }}>
-                        <li><Link to="/">Главная</Link></li>
-                        <li><Link to="/about">Обо мне</Link></li>
-                        <li><Link to="/services">Услуги</Link></li>
-                        <li><Link to="/portfolio">Мои работы</Link></li>
-                        <li><Link to="/contacts">Контакты</Link></li>
-                    </Navbar>
+                        <Navbar brand="Доктор Диана" right className="header">
+                            <li><Link to="/">Главная</Link></li>
+                            <li><Link to="/about">Обо мне</Link></li>
+                            <li><Link to="/services">Услуги</Link></li>
+                            <li><Link to="/portfolio">Мои работы</Link></li>
+                            <li><Link to="/contacts">Контакты</Link></li>
+                        </Navbar>
+                    </div >
                 </div >
-            </div >
+            </div>
         );
     }
 };
